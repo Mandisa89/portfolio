24ce04bf2493449ca5e4e3d6f528d87428329828
Mandisa89 <mandisa.macleod@gmail.com>
2024-11-28 15:59:27 +0200

    Updated Projects: Added All About Embroidery as featured project.

--- ate/index.js
+++ ate/index.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import methodOverride from "method-override";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let posts = [];
 
